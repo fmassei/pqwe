f3d0054e7f5ec9752ea4cad01e3bcbd8f027c701
FtM <fmassei@gmail.com>
2015-03-19 17:59:33 +0100

    - view path exposed in ControllerHTML

--- src/pqwe/Controller/ControllerHTML.php
+++ src/pqwe/Controller/ControllerHTML.php
@@ -6,28 +6,31 @@ use pqwe\View\IView;
 
 class ControllerHTML extends ControllerBase {
     protected $layoutView;
-    protected $viewFolder;
+    protected $viewFolderName;
+    protected $viewFolderPath;
     protected $layoutFile;
 
     private $actualDir;
 
     public function __construct($serviceManager) {
         parent::__construct($serviceManager);
-        $this->viewFolder = 'view';
+        $this->viewFolderName = 'view';
         $this->layoutFile = 'layout.phtml';
     }
     public function preAction($routeMatch) {
         $this->layoutView = new View();
         $this->actualDir =
             \pqwe\Utils\Namespaces::getFirst($routeMatch['controller']);
-        $fpath = \pqwe\Utils\Files::makePath($this->actualDir,
-                                        $this->viewFolder, $this->layoutFile);
+        $this->viewFolderPath = \pqwe\Utils\Files::makePath($this->actualDir,
+                                                $this->viewFolderName);
+        $fpath = \pqwe\Utils\Files::makePath($this->viewFolderPath,
+                                             $this->layoutFile);
         $this->layoutView->setViewFile($fpath);
     }
     public function postAction(IView $view, $action) {
         if ($view->isEmpty()) {
-            $fpath = \pqwe\Utils\Files::makePath($this->actualDir,
-                                        $this->viewFolder, $action.'.phtml');
+            $fpath = \pqwe\Utils\Files::makePath($this->viewFolderPath,
+                                                 $action.'.phtml');
             $view->setViewFile($fpath);
         }
         $this->layoutView->content = $view->return_output();
