b1d7147678e1b66aaa0f1bd52cf46c91aca08300
FtM <fmassei@gmail.com>
2015-04-30 11:55:57 +0200

    - makePath fix

--- src/pqwe/Utils/Files.php
+++ src/pqwe/Utils/Files.php
@@ -3,7 +3,11 @@ namespace pqwe\Utils;
 
 class Files {
     public static function makePath() {
-        return join(DIRECTORY_SEPARATOR, func_get_args());
+        $arr = func_get_args();
+        foreach($arr as &$p)
+            if ($p[strlen($p)-1]==DIRECTORY_SEPARATOR)
+                $p = substr($p, 0, strlen($p)-1);
+        return join(DIRECTORY_SEPARATOR, $arr);
     }
 }
 
