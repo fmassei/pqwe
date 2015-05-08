d3f8aeb577d0069980d8f3be7864d494489d9cf1
FtM <fmassei@gmail.com>
2015-05-08 12:23:59 +0200

    - fragmented filepath utility

--- src/pqwe/Utils/Files.php
+++ src/pqwe/Utils/Files.php
@@ -1,6 +1,8 @@
 <?php
 namespace pqwe\Utils;
 
+use pqwe\Exception\PqweException;
+
 class Files {
     public static function makePath() {
         $arr = func_get_args();
@@ -8,6 +10,14 @@ class Files {
             if ($p[strlen($p)-1]==DIRECTORY_SEPARATOR)
                 $p = substr($p, 0, strlen($p)-1);
         return join(DIRECTORY_SEPARATOR, $arr);
+    }
+    public static function prepareFragmented($filename, $basePath, $nPrefix=3, $newDirMode=0777) {
+        $subfolder = substr($filename, 0, $nPrefix);
+        $path = self::makePath($basePath, $subfolder);
+        if (!is_dir($path))
+            if (!mkdir($path, $newDirMode))
+                throw new PqweException("could not create directory $path");
+        return self::makePath($path, $filename);
     }
 }
 
