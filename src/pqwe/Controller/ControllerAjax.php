985f84b7643b3262952c3e0d138dfdbc14f6a4a6
FtM <fmassei@gmail.com>
2015-05-31 15:35:28 +0200

    - controller documentation on the way

--- src/pqwe/Controller/ControllerAjax.php
+++ src/pqwe/Controller/ControllerAjax.php
@@ -1,13 +1,33 @@
 <?php
+/**
+ * ControllerAjax class
+ */
 namespace pqwe\Controller;
 
 use pqwe\View\View;
 use pqwe\View\IView;
 
+/**
+ * Controller specialized for AJAX actions
+ * @todo document this thing
+ */
 class ControllerAjax extends ControllerBase {
+    /**
+     * constructor
+     *
+     * @param \pqwe\ServiceManager\ServiceManager $serviceManager A
+     * ServiceManager instance
+     */
     public function __construct($serviceManager) {
         parent::__construct($serviceManager);
     }
+
+    /**
+     * generic action method. It creates a View and fills it with the execute()
+     * return value (or with an error response, in case of errors).
+     *
+     * @return \pqwe\View\View
+     */
     public function ajaxAction() {
         $view = new View();
         try {
@@ -18,6 +38,13 @@ class ControllerAjax extends ControllerBase {
         }
         return $view;
     }
+
+    /**
+     * execute an ajax action, taking the $_POST['action'] value and calling
+     * the appropriate method.
+     *
+     * @return string
+     */
     protected function execute() {
         if (($ajaxAction = $this->getPOSTdefault('action'))===null)
             throw new \Exception('invalid action');
@@ -27,6 +54,14 @@ class ControllerAjax extends ControllerBase {
         $response = call_user_func(array($this, $method));
         return $this->response(true, $response);
     }
+
+    /**
+     * build a proper response, returning a JSON string
+     *
+     * @param bool $success If we managed to fullfill the request
+     * @param string $response The response content
+     * @return string
+     */
     protected function response($success, $response) {
         return json_encode(array('requestSuccess' => $success,
                                  'response' => $response));
--- src/pqwe/Controller/ControllerXML.php
+++ src/pqwe/Controller/ControllerXML.php
@@ -1,19 +1,39 @@
 <?php
+/**
+ * ControllerXML class
+ */
 namespace pqwe\Controller;
 
 use pqwe\View\View;
 use pqwe\View\IView;
 
+/**
+ * Controller specialized for XML actions
+ */
 class ControllerXML extends ControllerBase {
+    /** @var \pqwe\View\View $layoutView View for the layout */
     protected $layoutView;
+    /** @var string $viewFolderName Name of the folder containing the views */
     protected $viewFolderName;
+    /** @var string $viewFolerPath Path of the folder containing the views */
     protected $viewFolderPath;
 
+    /** @var string $version XML file version */
     protected $version;
+    /** @var string $encoding XML file encoding */
     protected $encoding;
 
+    /** @var string $actualDir Path of the controller file, used internally */
     private $actualDir;
 
+    /**
+     * constructor
+     *
+     * @param \pqwe\ServiceManager\ServiceManager $serviceManager A
+     * ServiceManager instance
+     * @param string $version XML file version
+     * @param string $encoding XML file encoding
+     */
     public function __construct($serviceManager,
                                 $version="1.0", $encoding="UTF-8") {
         parent::__construct($serviceManager);
@@ -21,9 +41,34 @@ class ControllerXML extends ControllerBase {
         $this->version = $version;
         $this->encoding = $encoding;
     }
+
+    /**
+     * callback, called by the MVC object before the action method
+     *
+     * This overridden method sets up the layout view as an empty View
+     *
+     * @param \pqwe\Routing\RouteMatch $routeMatch The matched route.
+     * @return void
+     */
     public function preAction(&$routeMatch) {
         $this->layoutView = new View();
     }
+
+    /**
+     * callback, called by the MVC object after the action method
+     *
+     * This overridden method renders the passed $view and the $layoutView.
+     *
+     * + If $view is an empty IView, the default one will be used (a file
+     * called $action.".phtml" in the $viewFolderPath).
+     * + If the $layoutView is empty (as by default), if will be filled with
+     * the appropriate XML header.
+     *
+     * @param \pqwe\View\IView $view The View object returned by the action
+     * method.
+     * @param string $action Name of the called action
+     * @return void
+     */
     public function postAction(IView $view, $action) {
         if ($view->isEmpty()) {
             $fpath = \pqwe\Utils\Files::makePath($this->viewFolderPath,
