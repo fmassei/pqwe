fd8fcb2611381baa00a8c3f542dd134d80c0569a
FtM <fmassei@gmail.com>
2015-06-15 21:02:05 +0200

    - multiple file upload handling

--- src/pqwe/Form/FileUpload.php
+++ src/pqwe/Form/FileUpload.php
@@ -55,6 +55,50 @@ class FileUpload {
         return $file;
     }
     /**
+     * getFileObjIndexed
+     *
+     * Takes a $_FILES entry and perform various checks, like if there was
+     * an error during the transfer, if the mime type is correct, and so on.
+     * Multifile-upload version.
+     *
+     * @static
+     * @param string $fieldName The key of the $_FILES array
+     * @param int $idx The index inside the $_FILES arrays
+     * @param int $maxSize Maximum file size, 0 for no limit
+     * @param array $acceptedMimeTypes Array of mime types to accept,
+     * null for no checks
+     * @return array The raw $_FILES entry
+     * @throws PqweUploadException
+     */
+    public static function getFileObjIndexed($fieldName, $idx, $maxSize=0,
+                                             $acceptedMimeTypes=null)
+    {
+        $file = $_FILES[$fieldName];
+        if (!isset($file['error'][$idx]) || is_array($file['error'][$idx]))
+            throw new PqweUploadException('Invalid parameters.');
+        switch ($file['error'][$idx]) {
+            case UPLOAD_ERR_OK:
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                throw new PqweUploadException('No file sent.');
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                throw new PqweUploadException('Exceeded filesize limit [machine configuration].');
+            default:
+                throw new PqweUploadException('Unknown errors.');
+        }
+        if ($maxSize>0 && $file['size'][$idx]>$maxSize)
+            throw new PqweUploadException('Exceeded filesize limit [site coded].');
+        if ($acceptedMimeTypes!==null) {
+            $finfo = new \finfo(FILEINFO_MIME_TYPE);
+            if (($ext = array_search(
+                    $finfo->file($file['tmp_name'][$idx]),
+                    $acceptedMimeTypes, true))===false)
+            throw new PqweUploadException('Invalid file format.');
+        }
+        return $file;
+    }
+    /**
      * getUploadedFile
      *
      * like move_uploaded_file, but with extra checks.
@@ -76,6 +120,34 @@ class FileUpload {
             throw new PqweUploadException('Out file already exists');
         if (!move_uploaded_file($file['tmp_name'], $outFile))
             throw new PqweUploadException('Failed to move uploaded file.');
+    }
+
+    /**
+     * getUploadedFiles
+     *
+     * like move_uploaded_file, but with extra checks.
+     *
+     * @static
+     * @param string $fieldName The key in the $_FILES array
+     * @param array $outFiles The names of the final files
+     * @param int $maxSize Maximum file size, 0 for no limit
+     * @param array $acceptedMimeTypes Array of mime types to accept,
+     * null for no checks
+     * @return void
+     * @throws PqweUploadException
+     */
+    public static function getUploadedFile($fieldName, $outFiles, $maxSize=0,
+                                           $acceptedMimeTypes=null)
+    {
+        if (count($outFiles)<count($_FILES[$fieldName]['name']))
+            throw new PqweUploadException('Too few names');
+        for ($i=0; $i<count($_FILES[$fieldName]['name']); ++$i) {
+            $file = self::getFileObjIndexed($fieldName, $i, $maxSize, $acceptedMimeTypes);
+            if (file_exists($outFiles[$i]))
+                throw new PqweUploadException('Out file already exists');
+            if (!move_uploaded_file($file['tmp_name'][$i], $outFiles[$i]))
+                throw new PqweUploadException('Failed to move uploaded file.');
+        }
     }
 }
 
