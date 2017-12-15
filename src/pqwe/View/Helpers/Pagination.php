608f17e0556a7f5e3d421cfe4e86411e9c6481d3
ftm <fmassei@gmail.com>
2017-12-15 18:52:28 -0500

    pagination fix

--- src/pqwe/View/Helpers/Pagination.php
+++ src/pqwe/View/Helpers/Pagination.php
@@ -5,30 +5,37 @@
 namespace pqwe\View\Helpers;
 
 /**
- * Help sending messages through pages using the session.
+ * Help rendering pagination links
  *
- * For this class to work, session_start() has to be called before its usage.
+ * @param $nElements int Total number of elements
+ * @param $step int Number of elements to display in a page
+ * @param $currentPage int Current page number
+ * @param $baseUrl string Page URL to which the page number will be added
+ *                          (something like "/list?p=")
+ * @param $range int Range of pages to display around current page
+ * @param $class string Css classes for the container
+ * @return string
  */
 class Pagination {
-    public static function renderLinks($total, $step, $currentPage, $range=2, $class="pagination") {
+    public static function renderLinks($nElements, $step, $currentPage, $baseUrl, $range=2, $class="pagination") {
         $ret = '<div class="'.$class.'">';
-        $nPages = ceil($total/(double)$step);
-        if ($total>$step) {
+        $nPages = ceil($nElements/(double)$step);
+        if ($nElements>$step) {
             if ($currentPage>1) {
-                $ret .= '<a href="/admin/audit?p=1">&lt;&lt;</a> ';
-                $ret .= '<a href="/admin/audit?p='.($currentPage-1).'">&lt</a> ';
+                $ret .= '<a href="'.$baseUrl.'1">&lt;&lt;</a> ';
+                $ret .= '<a href="'.$baseUrl.''.($currentPage-1).'">&lt</a> ';
             }
             for ($i=$currentPage-$range; $i<($currentPage+$range)+1; ++$i) {
                 if ($i>0 && $i<$nPages) {
                     if ($i==$currentPage)
                         $ret .= " [<b>$i</b>] ";
                     else
-                        $ret .= '<a href="/admin/audit?p='.$i.'">'.$i.'</a> ';
+                        $ret .= '<a href="'.$baseUrl.''.$i.'">'.$i.'</a> ';
                 }
             }
             if ($currentPage!=$nPages) {
-                $ret .= '<a href="/admin/audit?p='.($currentPage+1).'">&gt;</a> ';
-                $ret .= '<a href="/admin/audit?p='.$nPages.'">&gt;&gt;</a> ';
+                $ret .= '<a href="'.$baseUrl.''.($currentPage+1).'">&gt;</a> ';
+                $ret .= '<a href="'.$baseUrl.''.$nPages.'">&gt;&gt;</a> ';
             }
         } else {
             $ret .= "[<b>1</b>]";
